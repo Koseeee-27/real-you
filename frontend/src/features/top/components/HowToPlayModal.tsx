405765572881168c42178dedd73660a1bdf20307
nightstar039 <uranus.264171@icloud.com>
2026-05-28 13:41:37 +0900

    refactor: トップ画面遊び方ポップアップ2枚目に電球、PC、音符、ハートのイラストを追加

--- frontend/src/features/top/components/HowToPlayModal.tsx
+++ frontend/src/features/top/components/HowToPlayModal.tsx
@@ -23,7 +23,13 @@ export const howToPlaySlides: JSX.Element[] = [
       <p>あなたらしさが、自然とあらわれます。</p>
     </div>
   </div>,
+
   <div key="page2" className="flex flex-col items-center mt-4">
+    <img src="/images/heart.png" alt="" className="absolute top-4 right-18 w-25" />
+    <img src="/images/light.png" alt="" className="absolute top-2 left-10 w-30" />
+    <img src="/images/PC.png" alt="" className="absolute bottom-1 right-10 w-30" />
+    <img src="/images/tunes.png" alt="" className="absolute bottom-5 left-9 w-28" />
+
     <h2 className="mb-1 text-6xl font-bold leading-relaxed">
       プレイ時間は約<span className="text-blue-500 font-extrabold  [-webkit-text-stroke:2px_#000] [-webkit-text-fill-color:#3b82f6]">5</span>分！
       <div className="absolute right-[17.5%] top-1/3 -translate-x-1/2 -translate-y-1/4 h-15 w-[175px] bg-blue-500 rounded-full opacity-30 [-z-10]" />
@@ -34,6 +40,7 @@ export const howToPlaySlides: JSX.Element[] = [
       <p>※ゲーム中、BGMや効果音が流れます</p>
     </div>
   </div>,
+
   <div key="page3" className="flex flex-col items-center justify-center text-center">
     <img src="/images/mbti/ISTP.png" alt="" className="absolute top-18 right-28 w-20" />
     <img src="/images/mbti/INFJ.png" alt="" className="absolute top-4 right-85 w-20" />
