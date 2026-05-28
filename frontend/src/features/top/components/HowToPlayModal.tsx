be0e94e234a22196647f922b5e1504bb98775bdc
nightstar039 <uranus.264171@icloud.com>
2026-05-28 13:29:00 +0900

    refactor: トップ画面遊び方ポップアップにMBTIキャラを追加

--- frontend/src/features/top/components/HowToPlayModal.tsx
+++ frontend/src/features/top/components/HowToPlayModal.tsx
@@ -4,6 +4,15 @@ import type { JSX } from 'react';
 
 export const howToPlaySlides: JSX.Element[] = [
   <div key="page1" className="flex flex-col items-center mt-0">
+    <img src="/images/mbti/INFP.png" alt="" className="absolute top-18 right-28 w-20" />
+    <img src="/images/mbti/ISFJ.png" alt="" className="absolute top-4 right-70 w-20" />
+    <img src="/images/mbti/ESFP.png" alt="" className="absolute top-3 left-65 w-20" />
+    <img src="/images/mbti/INTJ.png" alt="" className="absolute top-12 left-32 w-20" />
+    <img src="/images/mbti/ENTP.png" alt="" className="absolute bottom-20 right-30 w-20" />
+    <img src="/images/mbti/ISFP.png" alt="" className="absolute bottom-5 right-52 w-20" />
+    <img src="/images/mbti/ESTJ.png" alt="" className="absolute bottom-6 left-45 w-20" />
+    <img src="/images/mbti/ENFP.png" alt="" className="absolute bottom-25 left-20 w-20" />
+
     <h2 className="mb-1 text-7xl font-bold leading-relaxed">Real Youとは？</h2>
     <div className="mb-2 text-2xl leading-relaxed relative">
       <span className="relative z-10"><span className="text-blue-500 font-extrabold  [-webkit-text-stroke:1px_#000] [-webkit-text-fill-color:#3b82f6]">"本当のあなた"</span>が分かる、新しい性格診断アプリ。</span>
@@ -26,6 +35,15 @@ export const howToPlaySlides: JSX.Element[] = [
     </div>
   </div>,
   <div key="page3" className="flex flex-col items-center justify-center text-center">
+    <img src="/images/mbti/ISTP.png" alt="" className="absolute top-18 right-28 w-20" />
+    <img src="/images/mbti/INFJ.png" alt="" className="absolute top-4 right-85 w-20" />
+    <img src="/images/mbti/ESFJ.png" alt="" className="absolute top-3 left-85 w-20" />
+    <img src="/images/mbti/INTP.png" alt="" className="absolute top-12 left-32 w-20" />
+    <img src="/images/mbti/ENTJ.png" alt="" className="absolute bottom-20 right-30 w-20" />
+    <img src="/images/mbti/ISTJ.png" alt="" className="absolute bottom-5 right-88 w-20" />
+    <img src="/images/mbti/ESTP.png" alt="" className="absolute bottom-4 left-85 w-20" />
+    <img src="/images/mbti/ENFJ.png" alt="" className="absolute bottom-18 left-20 w-20" />
+
     <h2 className="text-5xl font-bold leading-relaxed">さあ！<span className="text-yellow-500 font-extrabold [-webkit-text-stroke:2px_#000] [-webkit-text-fill-color:#f59e0b]">”本当の自分”</span>に会いに行こう！</h2>
     <div className="absolute left-[38%] top-1/2 -translate-x-1/2 -translate-y-1/2 h-15 w-[300px] bg-yellow-500 rounded-full opacity-30 [-z-10]" />
   </div>,
